d7ea4f6813549b6b9052489079139b3b9a93dbfd
manas <manasshukla997@gmail.com>
2017-09-11 02:43:44 +0530

    slightly changed welcome

--- Welcome.php
+++ Welcome.php
@@ -7,9 +7,20 @@
 	session_start();
 	$myName = $_SESSION['current_name'];
 ?>
-<html>
-	<head><h1 align="center"> SUCCESS!! </h1></head
-	<body>
-		<p>Welcome, <?php echo "$myName";?></p>
-	</body>
+
+<html lang="en">
+<head>
+    <title>Logged In!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid bg-info">
+	<h1 align="center"> SUCCESS!! </h1>
+	<h3 class="text-success" align="center">Welcome, <?php echo "$myName";?></h3>
+</div>
+</body>
 </html>
